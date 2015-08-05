98122803f9d9587c76d0ddbb066eb42273848e54
Luyang Li <liluyang@gmail.com>
2015-08-05 03:53:16 -0700

    Start to work!

---
+++ src/com/fun/example/Welcome.java
@@ -0,0 +1,10 @@
+package com.fun.example;
+
+import org.testng.annotations.Test;
+
+public class Welcome {
+  @Test
+  public void test() {
+    System.out.println("Welcome to program4fun! You are on right track!");
+  }
+}
