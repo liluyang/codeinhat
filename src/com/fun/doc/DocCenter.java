c329cc3366707d806ba1a69585f2fbc4ca0e5baf
Luyang Li <liluyang@gmail.com>
2016-02-25 21:20:21 -0800

    Add DocCenter.

---
+++ src/com/fun/doc/DocCenter.java
@@ -0,0 +1,30 @@
+package com.fun.doc;
+
+/**
+ * The demo class to exhibit the power of javadoc. Try to use all javadoc features in this class.
+ * Try to use HTML elements, like <ul>, <li>, <p>.
+ * @author
+ */
+public class DocCenter {
+  public void getDocument(String filePath, String encoding) {
+    // Try to document parameters.
+    // @param
+  }
+  
+  public int getParameter(String name) {
+    // Try to document method return value.
+    // @return
+    // @see
+    return 0;
+  }
+  
+  public void runThread() throws Exception {
+    // Try to document exception.
+    // @exception
+  }
+  
+  public void oldMethod() {
+    // Try to document deprecated method.
+    // @deprecated
+  }
+}
