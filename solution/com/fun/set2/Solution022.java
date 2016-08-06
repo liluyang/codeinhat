eb50897293648e578aa1f6bd121924c49e0bd9e1
Luyang Li <liluyang@gmail.com>
2016-08-06 08:37:03 -0700

    Update pokemon

--- solution/com/fun/set2/Solution022.java
+++ solution/com/fun/set2/Solution022.java
@@ -7,14 +7,21 @@ import com.fun.datastructure.Pokemon;
 public class Solution022 extends Problem022 {
   @Override
   public int maxPoints(Pokemon pokemon, int balls) {
-    // 1. Catach pokemon with balls
+    int N = pokemon.getCandyForEvolve();
+
+    // 1. Catach pokemon with balls.
     int caught = pokemon.getCaught() + balls;
+    int candy = pokemon.getCandy() + balls * 3;
 
-    // 2. Evolve max number of pokemons
-    int evolved = caught / pokemon.getCandyForEvolve();
+    // 2. Evolve max number of pokemons.
+    int evolved = Math.min(caught, candy / N);
     caught -= evolved;
+    candy -= evolved * N;
 
-    return 0;
+    // 3. Transfer and evolve as much as possible.
+    evolved += Math.min(caught, (caught + candy) / (N + 1));
+
+    return evolved * 500;
   }
 
   @Test
--- src/com/fun/set2/Problem022.java
+++ src/com/fun/set2/Problem022.java
@@ -19,6 +19,31 @@ public class Problem022 {
 
   @Test
   public void test2() {
+    Pokemon pidgey = new Pokemon("Pidgey", 6, 60, 12);
+    assertEquals(maxPoints(pidgey, 100), 35 * 500);
+  }
 
+  @Test
+  public void test3() {
+    Pokemon pidgey = new Pokemon("Pidgey", 1, 11, 12);
+    assertEquals(maxPoints(pidgey, 0), 0 * 500);
+  }
+
+  @Test
+  public void test4() {
+    Pokemon pidgey = new Pokemon("Pidgey", 12, 0, 12);
+    assertEquals(maxPoints(pidgey, 0), 0 * 500);
+  }
+
+  @Test
+  public void test5() {
+    Pokemon pidgey = new Pokemon("Eevee", 12, 24, 25);
+    assertEquals(maxPoints(pidgey, 200), 32 * 500);
+  }
+
+  @Test
+  public void test6() {
+    Pokemon pidgey = new Pokemon("MagiKarp", 85, 85 * 3, 400);
+    assertEquals(maxPoints(pidgey, 100), 1 * 500);
   }
 }
