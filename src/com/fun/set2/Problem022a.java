2a928e729943adfe0178ff90c1697447e3e1703a
Luyang Li <liluyang@gmail.com>
2016-08-21 05:57:11 -0700

    fix 22a bug.

--- src/com/fun/set2/Problem022a.java
+++ src/com/fun/set2/Problem022a.java
@@ -23,7 +23,20 @@ public class Problem022a {
   public void test2() {
     Pokemon pidgey = new Pokemon("Pidgey", 0, 0, 12);
     Pokemon rattata = new Pokemon("Rattata", 12, 24, 25);
-    Pokemon magikarp = new Pokemon("MagiKarp", 85, 85 * 3, 100);
-    assertEquals(maxPoints(new Pokemon[] { pidgey, rattata, magikarp }, 100), 34 * 500);
+    assertEquals(maxPoints(new Pokemon[] { pidgey, rattata }, 100), 31 * 500);
+  }
+
+  @Test
+  public void test3() {
+    Pokemon magikarp = new Pokemon("MagiKarp", 85, 85 * 3, 400);
+    assertEquals(maxPoints(new Pokemon[] { magikarp }, 100), 500);
+  }
+
+  @Test
+  public void test4() {
+    Pokemon pidgey = new Pokemon("Pidgey", 0, 0, 12);
+    Pokemon rattata = new Pokemon("Rattata", 12, 24, 25);
+    Pokemon magikarp = new Pokemon("MagiKarp", 85, 85 * 3, 400);
+    assertEquals(maxPoints(new Pokemon[] { pidgey, rattata, magikarp }, 100), 31 * 500);
   }
 }
