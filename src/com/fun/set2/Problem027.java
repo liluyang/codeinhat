e648bb2bba0dcc0058800d3ab71c7361e6a5b2d3
Luyang Li <liluyang@gmail.com>
2017-01-14 06:20:54 -0800

    Add problem 27.

---
+++ src/com/fun/set2/Problem027.java
@@ -0,0 +1,13 @@
+package com.fun.set2;
+
+public class Problem027 {
+  public static void main(String[] args) {
+    String inFile = "data/set2/encrypted.png";
+    String outFile = "data/set2/decrypted2.png";
+    new Problem027().convertImage(inFile, outFile);
+  }
+
+  private void convertImage(String inFile, String outFile) {
+    // TODO: convert inFile to outFile
+  }
+}
