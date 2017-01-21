5b2662c7e5d51277b3830d6c08caa56ec3aeb6c1
Luyang Li <liluyang@gmail.com>
2017-01-21 06:27:15 -0800

    .

--- solution/com/fun/set2/Solution028.java
+++ solution/com/fun/set2/Solution028.java
@@ -6,6 +6,25 @@ public class Solution028 extends Problem028 {
   @Override
   public int[] numStream(int[] a) {
     int[] res = new int[2];
+    int last = a[0];
+    int count = 1;
+    for (int i = 1; i < a.length; i++) {
+      if (a[i] == last) {
+        count++;
+      } else {
+        if (count > res[1]) {
+          res[0] = last;
+          res[1] = count;
+        }
+        last = a[i];
+        count = 1;
+      }
+    }
+
+    if (count > res[1]) {
+      res[0] = last;
+      res[1] = count;
+    }
 
     return res;
   }
