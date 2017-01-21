52667be93bf66eea3e0120b485d36b7d7cb52c77
Luyang Li <liluyang@gmail.com>
2017-01-21 06:20:11 -0800

    update problem 28.

---
+++ solution/com/fun/set2/Solution028.java
@@ -0,0 +1,17 @@
+package com.fun.set2;
+
+import org.testng.annotations.Test;
+
+public class Solution028 extends Problem028 {
+  @Override
+  public int[] numStream(int[] a) {
+    int[] res = new int[2];
+
+    return res;
+  }
+
+  @Test
+  public void test() {
+
+  }
+}
--- src/com/fun/set2/Problem028.java
+++ src/com/fun/set2/Problem028.java
@@ -2,6 +2,8 @@ package com.fun.set2;
 
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.Test;
+
 public class Problem028 {
   public int[] numStream(int[] a) {
     int[] res = new int[2];
@@ -9,26 +11,31 @@ public class Problem028 {
     return res;
   }
 
+  @Test
   public void test1() {
     int[] a = new int[] { 1 };
     assertEquals(numStream(a), new int[] { 1, 1 });
   }
 
+  @Test
   public void test2() {
     int[] a = new int[] { 1, 1, 2, 2, 2, 563, 78, 43, 43, 43, 21, 2, 2, 2, 2, 2, 5 };
     assertEquals(numStream(a), new int[] { 2, 5 });
   }
 
+  @Test
   public void test3() {
     int[] a = new int[] { 1, 1, 2, 2, 2 };
     assertEquals(numStream(a), new int[] { 2, 3 });
   }
 
+  @Test
   public void test4() {
     int[] a = new int[] { 1, 1, 1, 2, 2, 3, 3, 4, 4 };
     assertEquals(numStream(a), new int[] { 1, 3 });
   }
 
+  @Test
   public void test5() {
     int[] a = new int[] { 1, 2, 1, 2, 1 };
     assertEquals(numStream(a), new int[] { 1, 1 });
